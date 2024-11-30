234ecdaabc37c972c842ced512a31fbe213c5335
Shadow-lab528 <masterlegendary063@gmail.com>
2024-11-30 02:09:19 +0100

    Update config.js

--- config.js
+++ config.js
@@ -7,8 +7,8 @@
 */
 
 global.prefa = ['','!','.',',','🐤','🗿']
-global.owner = ['2348187637779']
-global.ownMain = '2348187637779'
+global.owner = ['2348169546065']
+global.ownMain = '2348169546065'
 global.NamaOwner = 'Es Teams Tech' //gausah diganti
 global.sessionName = 'sessionnya'
 global.connect = true // Ubah Ke False Jika Ingin Menggunakan Qr Code
